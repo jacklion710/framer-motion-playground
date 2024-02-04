dfa8d25a4d6ca87bad0b00338c7b34b920601794
jacklion710 <jack.lion710@gmail.com>
2024-02-04 15:28:33 -0500

    Changed name to Motion & wrapped in ChakraProvider

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
+"use client"
 import React from 'react';
-import MotionComponent from '../components/Motion';
-import { Box, Button } from '@chakra-ui/react';
+import Motion from '../components/Motion';
+import { 
+  Box, 
+  Button,
+  ChakraProvider
+} from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <main>
-      <MotionComponent animationType="fade" duration={1} easing="easeOut">
+    <ChakraProvider>
+      <Motion animationType="fade" duration={1} easing="easeOut">
         <Box p={5} shadow="md" borderWidth="1px">
           <p>Content inside a box</p>
         </Box>
-      </MotionComponent>
+      </Motion>
 
-      <MotionComponent animationType="slide" duration={0.5} easing="easeIn">
+      <Motion animationType="slide" duration={0.5} easing="easeIn">
         <Button colorScheme="blue">Click Me</Button>
-      </MotionComponent>
+      </Motion>
 
-    </main>
+    </ChakraProvider>
   );
 }
