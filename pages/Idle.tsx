1851268868b549b014c94a993ecb54c29fa8fb14
jacklion710 <jack.lion710@gmail.com>
2024-02-04 19:15:13 -0500

    Placeholders for upcoming collections

---
+++ pages/Idle.tsx
@@ -0,0 +1,27 @@
+import React, { 
+    useState 
+} from 'react';
+import MotionFade from '../components/MotionFade';
+import MotionRotate from '../components/MotionRotate';
+import MotionScale from '../components/MotionScale';
+import MotionSlide from '../components/MotionSlide';
+import { 
+    VStack, 
+    ChakraProvider, 
+    Heading, 
+} from '@chakra-ui/react';
+
+const Idle = () => {
+
+  return (
+    <ChakraProvider>
+      <VStack spacing={20} mt={20} align="center" justify="center" height="100vh">
+
+       <Heading>This collection is a work in progress.</Heading>
+
+      </VStack>
+    </ChakraProvider>
+  );
+};
+
+export default Idle;
