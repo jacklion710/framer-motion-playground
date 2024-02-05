d30f956220c41705efde2068d5807d111f99cc3b
jacklion710 <jack.lion710@gmail.com>
2024-02-04 19:44:41 -0500

    Fancy effects

--- pages/Fancy.tsx
+++ pages/Fancy.tsx
@@ -1,35 +1,49 @@
 import React, { 
     useState 
 } from 'react';
-import MotionFade from '../components/MotionFade';
-import MotionRotate from '../components/MotionRotate';
-import MotionScale from '../components/MotionScale';
-import MotionSlide from '../components/MotionSlide';
+import MotionFlip from '../components/fancy/MotionFlip';
 import { 
     VStack, 
     ChakraProvider, 
-    Heading, 
     Link,
     Button,
-    Box
+    Box,
+    Image,
+    Center
 } from '@chakra-ui/react';
 
 const Fancy = () => {
+    const [flipTrigger, setFlipTrigger] = useState(false);
+  
+    return (
+      <ChakraProvider>
+        <Box position="relative" top={5} left={4}>
+          <Link href="/AnimMenu">
+            <Button colorScheme="blue">Animations Menu</Button>
+          </Link>
+        </Box>
+        <VStack spacing={20} mt={20} align="center" justify="center" height="100vh">
 
-  return (
-    <ChakraProvider>
-      <Box position="relative" top={5} left={4}>
-        <Link href="/AnimMenu">
-          <Button colorScheme="blue">Animations Menu</Button>
-        </Link>
-      </Box>
-      <VStack spacing={20} mt={20} align="center" justify="center" height="100vh">
+            <Center>
+                <VStack spacing={10}>
+                    <MotionFlip
+                        duration={1000}
+                        easing="easeInOut"
+                        trigger={flipTrigger}
+                        flipDirection="horizontal"
+                    >
+                        <Image src="/next.svg" alt="Next.js logo" boxSize="150px" />
+                    </MotionFlip>
+                    <Button colorScheme="blue" onClick={() => setFlipTrigger(!flipTrigger)}>
+                        Trigger Flip
+                    </Button>
+                </VStack>
+            </Center>
 
-       <Heading>This collection is a work in progress.</Heading>
-
-      </VStack>
-    </ChakraProvider>
-  );
-};
-
-export default Fancy;
+        </VStack>
+      </ChakraProvider>
+    );
+  };
+  
+  export default Fancy;
+  
