bb2f0748f94d3a842a6c2388b732dc0d21e52dcb
jacklion710 <jack.lion710@gmail.com>
2024-02-04 23:42:10 -0500

    Idle animations

--- pages/Idle.tsx
+++ pages/Idle.tsx
@@ -1,21 +1,20 @@
-import React, { 
-    useState 
-} from 'react';
-import MotionFade from '../components/standard/MotionFade';
-import MotionRotate from '../components/standard/MotionRotate';
-import MotionScale from '../components/standard/MotionScale';
-import MotionSlide from '../components/standard/MotionSlide';
+import React from 'react';
+import MotionBreathing from '../components/idle/MotionBreathing';
+import MotionFloat from '../components/idle/MotionFloat';
+import MotionPulse from '../components/idle/MotionPulse';
+import MotionSpin from '../components/idle/MotionSpin';
+
 import { 
     VStack, 
     ChakraProvider, 
-    Heading, 
+    Image, 
     Link,
     Button,
-    Box
+    Box,
+    Center
 } from '@chakra-ui/react';
 
 const Idle = () => {
-
   return (
     <ChakraProvider>
       <Box position="relative" top={5} left={4}>
@@ -23,10 +22,46 @@ const Idle = () => {
           <Button colorScheme="blue">Animations Menu</Button>
         </Link>
       </Box>
-      <VStack spacing={20} mt={20} align="center" justify="center" height="100vh">
+      <VStack spacing={20} align="center" justify="center" height="100vh">
+        <Center>
+            <VStack spacing={10}>
+                <MotionBreathing
+                    duration={2}
+                    trigger={true}
+                    intensity={.4}
+                >
+                    <Image src="/next.svg" alt="Next.js logo" boxSize="150px" />
+                </MotionBreathing>
 
-       <Heading>This collection is a work in progress.</Heading>
+                <MotionFloat
+                    duration={2}
+                    easing="easeInOut"
+                    trigger={true}
+                    floatDistance={20}
+                    direction="up-down"
+                >
+                    <Image src="/vercel.svg" alt="Vercel logo" boxSize="150px" />
+                </MotionFloat>
 
+                <MotionPulse
+                    duration={1}
+                    easing="easeInOut"
+                    trigger={true}
+                    scaleMin={0.9}
+                    scaleMax={1.1}
+                >
+                    <Image src="/next.svg" alt="Next.js logo" boxSize="150px" />
+                </MotionPulse>
+
+                <MotionSpin
+                    duration={10}
+                    easing="linear"
+                    trigger={true}
+                >
+                    <Image src="/vercel.svg" alt="Vercel logo" boxSize="150px" />
+                </MotionSpin>
+            </VStack>
+        </Center>
       </VStack>
     </ChakraProvider>
   );
